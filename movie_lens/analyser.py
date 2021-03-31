ad43e34ea8effe519f80c0617d78fd8b4a82e35b
CSCSCS84 <christoph.stettin@gmx.net>
2021-03-31 13:48:28 +0200

    clean code

--- movie_lens/analyser.py
+++ movie_lens/analyser.py
@@ -1,7 +1,3 @@
-import pandas as pd
-import numpy as np
-import re
-
 def analyse_genome_scores(genome_scores):
     print(genome_scores.info(memory_usage="deep"))
     print(genome_scores.head(20))
@@ -12,6 +8,7 @@ def analyse_genome_scores(genome_scores):
     tag_id = genome_scores.loc[:, "tagId"]
     print(tag_id.is_unique)
 
+
 def analyse_genomeTags(genome_tags):
     print(genome_tags.info())
     print(genome_tags.describe())
@@ -19,3 +16,23 @@ def analyse_genomeTags(genome_tags):
     print(genome_tags.columns)
     print(genome_tags.isna().sum())
     print(genome_tags.head(10))
+
+
+def analyse_link(link):
+    print(link.info(memory_usage="deep"))
+    print(link.head())
+    print(link.isna().sum())
+
+
+def analyse_movie(movie):
+    print(movie.info(memory_usage="deep"))
+    print(movie.head(10))
+    print(movie.isna().sum())
+    print(movie.head())
+
+
+def analyse_ratings(ratings):
+    print(ratings.head())
+    print(ratings.info(memory_usage="deep"))
+    print(ratings.isna().sum())
+    print(ratings.head(10))
--- movie_lens/reader_and_cleaner.py
+++ movie_lens/reader_and_cleaner.py
@@ -1,15 +1,54 @@
 import pandas as pd
-import numpy as np
 import re
 
 
-def read_genome_scores():
-    genome_scores=pd.read_csv("data//genome_scores.csv", nrows=100)
+def read_and_clean_genome_scores():
+    genome_scores = pd.read_csv("data//genome_scores.csv")
+    genome_scores.dropna()
     return genome_scores
 
-def read_genome_tags():
-    genome_tags = pd.read_csv("data//genome_tags.csv")
-    rename_dict = {genome_tags.columns.values[0]: "KindOfMovieID", genome_tags.columns.values[1]: "Kind Of Movie"}
 
+def read_and_clean_genome_tags():
+    genome_tags = pd.read_csv("data//genome_tags.csv")
+    genome_tags.dropna()
+    rename_dict = {genome_tags.columns.values[0]: "KindOfMovieID", genome_tags.columns.values[1]: "Kind Of Movie"}
     genome_tags.rename(mapper=rename_dict, axis=1, inplace=True)
     return genome_tags
+
+
+def read_and_clean_link():
+    link = pd.read_csv("data//link.csv")
+    link.dropna()
+    return link
+
+
+def read_and_clean_movie():
+    movie = pd.read_csv("data//movie.csv")
+    movie.dropna()
+
+    movie.loc[:, "year"] = movie.loc[:, "title"].apply(extract_year_and_name_of_title)
+    movie.loc[:, "year"] = pd.to_datetime(movie.loc[:, "year"], format='%Y')
+    return movie
+
+
+def read_and_clean_ratings():
+    ratings = pd.read_csv("data//rating.csv")
+    ratings.dropna()
+    ratings.loc[:, "timestamp"] = pd.to_datetime(ratings.loc[:, "timestamp"])
+    ratings.loc[:, "timestamp"] = ratings.loc[:, "timestamp"].dt.year
+    ratings.loc[:, "timestamp"] = pd.to_datetime(ratings.loc[:, "timestamp"], format='%Y')
+    return ratings
+
+
+def extract_year_and_name_of_title(title):
+    year = title.split("(")[-1].strip()
+    year = re.sub("[^0-9]", "", year)
+
+    try:
+        int(year)
+    except ValueError:
+        return
+    if int(year) > 2022 or int(year) < 1880:
+        print("Not a valid year: {} {}".format(title, year))
+        return
+    return year
