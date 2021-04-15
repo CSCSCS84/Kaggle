e6e2f101a7f526093de8cf903bfb7c3e99b19ae0
CSCSCS84 <christoph.stettin@gmx.net>
2021-04-15 13:54:27 +0200

    add some basic plots

--- movie_lens/first_analysis.py
+++ movie_lens/first_analysis.py
@@ -6,39 +6,70 @@ import seaborn as sns
 from movie_lens import reader_and_cleaner as rc
 from movie_lens import analyser as an
 import matplotlib.pyplot as plt
+import sys
 
 start = default_timer()
 pd.options.display.max_columns = 40
+nrows = 100000
+#nrows=sys.maxsize
+genome_scores = rc.read_and_clean_genome_scores(nrows)
+genome_tags = rc.read_and_clean_genome_tags(nrows)
+link = rc.read_and_clean_link(nrows)
+movie = rc.read_and_clean_movie(nrows)
+ratings = rc.read_and_clean_ratings(nrows)
 
-genome_scores = rc.read_and_clean_genome_scores()
-genome_tags = rc.read_and_clean_genome_tags()
-link = rc.read_and_clean_link()
-movie = rc.read_and_clean_movie()
-ratings = rc.read_and_clean_ratings()
 
-an.analyse_genome_scores(genome_scores)
-an.analyse_genomeTags(genome_tags)
-an.analyse_genomeTags(link)
-an.analyse_movie(movie)
-an.analyse_ratings(ratings)
+def analyse_datasets():
+    an.analyse_genome_scores(genome_scores)
+    an.analyse_genomeTags(genome_tags)
+    an.analyse_genomeTags(link)
+    an.analyse_movie(movie)
+    an.analyse_ratings(ratings)
+
+
+analyse_datasets()
+genres = rc.get_all_genres(movie)
+cumsum_of_movies=rc.cumsum_of_movies(movie)
+movie = rc.extract_genres(movie)
+genres_cumsum = rc.group_by_year_and_genre(movie, genres)
+
+print(cumsum_of_movies.tail(10))
+
+
+def number_of_movies_and_ratings():
+    movies_per_year = movie.groupby(by="year").count()
+    ratings_per_year = ratings.groupby(by="timestamp").count()
+    fig, ax1 = plt.subplots(figsize=(10, 5))
+    col = movies_per_year.columns[0]
+    g = sns.lineplot(x=movies_per_year.index, y=movies_per_year.loc[:, col])
+    g.set(xlabel='Year', ylabel='Number of Movies released', title="Number of Movies per Year")
+    col2 = ratings_per_year.columns[1]
+    ax2 = ax1.twinx()
+    g2 = sns.lineplot(x=ratings_per_year.index, y=ratings_per_year.loc[:, col2], ax=ax2, color="green")
+    g2.set(xlabel='Year', ylabel='Number of Ratings')
+
 
 # Explore the data with some basic plots
 # number of movies and ratings per year:
 # year muss nach timestamp gecastet werden
-movies_per_year = movie.groupby(by="year").count()
-ratings_per_year = ratings.groupby(by="timestamp").count()
 
-fig, ax1 = plt.subplots(figsize=(10, 5))
+# number_of_movies_and_ratings()
 
-col = movies_per_year.columns[0]
-g = sns.lineplot(x=movies_per_year.index, y=movies_per_year.loc[:, col])
-g.set(xlabel='Year', ylabel='Number of Movies released', title="Number of Movies per Year")
-
-col2 = ratings_per_year.columns[1]
-ax2 = ax1.twinx()
-g2 = sns.lineplot(x=ratings_per_year.index, y=ratings_per_year.loc[:, col2], ax=ax2, color="green")
-g2.set(xlabel='Year', ylabel='Number of Ratings')
+# cumsum of genre per year
+# rc.group_by_year_and_genre(movie,genres)
 
 end = default_timer()
 print(end - start)
+plt.style.use('seaborn')
+print(cumsum_of_movies.info())
+genres_cumsum.plot.area(stacked=True,figsize=(10,5))
+print(cumsum_of_movies.head(10))
+plt.plot(cumsum_of_movies.loc[:,"cumsum of movies"],marker='o', markerfacecolor='black',markersize=5,label="CumSum of Movies")
+
+plt.set_cmap('gray')
+#plt.figure(figsize=(15,5))
+plt.xlabel('Year', fontsize=12)
+plt.ylabel('Cumulative number of movies-genre', fontsize=12)
+plt.title('Total movies-genre',fontsize=17)
+plt.legend(loc="upper left", ncol=2)
 plt.show()
--- movie_lens/reader_and_cleaner.py
+++ movie_lens/reader_and_cleaner.py
@@ -2,28 +2,28 @@ import pandas as pd
 import re
 
 
-def read_and_clean_genome_scores():
-    genome_scores = pd.read_csv("data//genome_scores.csv")
+def read_and_clean_genome_scores(nrwosValue):
+    genome_scores = pd.read_csv("data//genome_scores.csv", nrows=nrwosValue)
     genome_scores.dropna()
     return genome_scores
 
 
-def read_and_clean_genome_tags():
-    genome_tags = pd.read_csv("data//genome_tags.csv")
+def read_and_clean_genome_tags(nrwosValue):
+    genome_tags = pd.read_csv("data//genome_tags.csv", nrows=nrwosValue)
     genome_tags.dropna()
     rename_dict = {genome_tags.columns.values[0]: "KindOfMovieID", genome_tags.columns.values[1]: "Kind Of Movie"}
     genome_tags.rename(mapper=rename_dict, axis=1, inplace=True)
     return genome_tags
 
 
-def read_and_clean_link():
-    link = pd.read_csv("data//link.csv")
+def read_and_clean_link(nrwosValue):
+    link = pd.read_csv("data//link.csv", nrows=nrwosValue)
     link.dropna()
     return link
 
 
-def read_and_clean_movie():
-    movie = pd.read_csv("data//movie.csv")
+def read_and_clean_movie(nrwosValue):
+    movie = pd.read_csv("data//movie.csv", nrows=nrwosValue)
     movie.dropna()
 
     movie.loc[:, "year"] = movie.loc[:, "title"].apply(extract_year_and_name_of_title)
@@ -31,8 +31,8 @@ def read_and_clean_movie():
     return movie
 
 
-def read_and_clean_ratings():
-    ratings = pd.read_csv("data//rating.csv")
+def read_and_clean_ratings(nrwosValue):
+    ratings = pd.read_csv("data//rating.csv", nrows=nrwosValue)
     ratings.dropna()
     ratings.loc[:, "timestamp"] = pd.to_datetime(ratings.loc[:, "timestamp"])
     ratings.loc[:, "timestamp"] = ratings.loc[:, "timestamp"].dt.year
@@ -52,3 +52,55 @@ def extract_year_and_name_of_title(title):
         print("Not a valid year: {} {}".format(title, year))
         return
     return year
+
+
+def extract_genre_from_row(genres_from_movie, genres):
+    genres_from_row = genres_from_movie.split("|")
+    print(genres_from_row)
+    genres.update(genres_from_row)
+
+
+def get_all_genres(movies):
+    genres_list = movies.genres.str.split('|').to_list()
+    s = set()
+    [s.update(e) for e in genres_list]
+    return s
+
+
+def extract_genres(movies):
+    dummies = movies.genres.str.get_dummies().astype(bool)
+
+    # print(dummies)
+    # print(type(dummies))
+    movies = movies.join(dummies)
+    # print(movies.head(10))
+    movies.drop("genres", inplace=True, axis=1)
+    # print(movies)
+    return movies
+    # movies.loc[:,"genres"].apply(extract_genre_from_row,genres=genres_set)
+
+    # return s
+
+
+def group_by_year_and_genre(movies, genres):
+    df = pd.DataFrame(index=movies["year"].unique())
+    df.sort_index(inplace=True)
+    print(df.info())
+    # grouped_movies=movies.groupyby(["year"])
+    print(movies.info())
+    for genre in genres:
+        df[genre]=movies.loc[:, ["year", genre]].groupby(["year"]).sum()
+        df.loc[:,genre]=df.loc[:,genre].cumsum()
+        # print(movies[genre])
+    return df
+    #print(df.head())
+
+def cumsum_of_movies(movies):
+    cumsum_of_movies=pd.DataFrame(index=movies["year"].unique())
+    cumsum_of_movies.sort_index(inplace=True)
+    #cumsum_of_movies["movies_per_year"]=
+    #print("movies per year")
+    #print(movies.groupby("year").count())
+    cumsum_of_movies["cumsum of movies"]=movies.groupby("year").count().loc[:,"movieId"]
+    cumsum_of_movies.loc[:, "cumsum of movies"] = cumsum_of_movies.loc[:, "cumsum of movies"].cumsum()
+    return cumsum_of_movies
